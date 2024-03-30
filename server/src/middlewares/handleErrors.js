45ad47d2add6d2a55cc152cb53818849ef6250d1
Haileiyesus <Haileiyesus29@gmail.com>
2024-03-30 18:11:33 +0300

    new backend with typescript

--- server/src/middlewares/handleErrors.js
+++
@@ -1,7 +0,0 @@
-import { logger } from '../../config/_logger.js'
-
-export async function handleErrors(err, req, res, next) {
-   logger.error(err.message)
-   console.trace(err)
-   res.status(err.status || 500).json(err)
-}
